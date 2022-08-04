de4ac5e55cb8f646193bf7106ff054fc29fc9a56
Yingyu <lin.yingyu@northeastern.edu>
2022-08-04 13:53:25 -0700

    Combine the funiture.controller and movies.controller

--- api/furniture.controller.js
+++ api/furniture.controller.js
@@ -1,8 +1,8 @@
 import FurnitureDAO from "../dao/furnitureDAO.js";
 
-export default class MoviesController {
+export default class FurnitureController {
 
-    static async apiGetMovies(req, res, next) {
+    static async apiGetFurnitureCollection(req, res, next) {
         const furniturePerPage = req.query.furniturePerPage ?
             parseInt(req.query.furniturePerPage) : 20;
         const page = req.query.page ? parseInt(req.query.page) : 0;
@@ -27,7 +27,7 @@ export default class MoviesController {
         res.json(response);
     }
 
-    static async apiGetMovieById(req, res, next) {
+    static async apiGetFurnitureById(req, res, next) {
         try {
             let id = req.params.id || {};
             let furniture = await FurnitureDAO.getFurnitureById(id);
@@ -50,5 +50,39 @@ export default class MoviesController {
             console.log(`API, ${e}`);
             res.status(500).json({ error: e});
         }
+    }
+
+    static async apiUploadItem(req, res, next) {
+      try {
+        const imageUrl = req.body.imageUrl;
+        const name = req.body.name;
+        const price = req.body.price;
+        const description = req.body.description;
+        const user = req.body.user;
+        const category = req.body.category;
+        const condition = req.body.condition;
+        const date = new Date();
+  
+        const uploadItemResponse = await FurnitureDAO.uploadItem(
+          user,
+          imageUrl,
+          name,
+          price,
+          category,
+          description,
+          condition,
+          date
+        );
+  
+        let { error } = uploadItemResponse;
+        if (error) {
+          res.status(500).json({ error: "Unable to upload item for sell." });
+        } else {
+          res.json({ status: "success" });
+        }
+      } catch (e) {
+        console.log("Reached here");
+        res.status(500).json({ error: e.message });
+      }
     }
 }

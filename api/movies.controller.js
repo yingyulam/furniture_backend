e2d834ec9b53506e07ef050c9ae25e60c8819d0f
Yingyu <lin.yingyu@northeastern.edu>
2022-08-04 13:33:36 -0700

    Change movies into furniture

--- api/movies.controller.js
+++ api/movies.controller.js
@@ -18,7 +18,7 @@ export default class MoviesController {
             FurnitureDAO.getMovies({ filters, page, moviesPerPage });
         
         let response = {
-            movies: moviesList,
+            furniture: moviesList,
             page: page,
             filters: filters,
             entries_per_page: moviesPerPage,
